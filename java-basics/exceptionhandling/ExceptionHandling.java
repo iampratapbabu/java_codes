bce688eff065fda42e71ff585a2c099072af48af
Tej Pratap <bankebihari5463@gmail.com>
2024-04-26 00:19:34 +0530

    added exception handling

---
+++ java-basics/exceptionhandling/ExceptionHandling.java
@@ -0,0 +1,27 @@
+package exceptionhandling;
+
+public class ExceptionHandling {
+
+    public static void main(String[] args){
+        System.out.println("exception handling fun started");  
+        method1();
+        System.out.println("main method ended");
+    }
+
+    private static void method1() {
+        System.out.println("method 1 started");
+        method2();
+    }
+
+    private static void method2() {
+        try{
+            String str = null;
+            str.length();
+            System.out.println("method 2 ended");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+}
