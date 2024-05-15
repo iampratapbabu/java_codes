8df76e9ba968c1bbde5b103b4a3dde29e9be586a
Tej Pratap <bankebihari5463@gmail.com>
2024-05-16 00:48:52 +0530

    change

--- java-basics/generics/MainClass.java
+++ java-basics/generics/MainClass.java
@@ -1,7 +1,23 @@
 package generics;
 
+
 public class MainClass {
+
     public static void main(String[] args){
-        System.out.println("Generics started");
+        MyCustomList<String> slist = new MyCustomList<String>();
+        slist.addElement("ele 1");
+        slist.addElement("ele 2");
+
+        MyCustomList<Integer> intlist = new MyCustomList<Integer>();
+        intlist.addElement(1);
+        intlist.addElement(2);
+
+        System.out.println(slist.toString());
+        System.out.println(intlist.toString());
+
+        System.out.println(slist.getValue(0));
+        System.out.println(intlist.getValue(0));
+
+        
     }
 }
