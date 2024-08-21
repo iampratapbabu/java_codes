d50e805e6031fa1b11574dfd8cbb3f176c76fed9
Tej Pratap <bankebihari5463@gmail.com>
2024-08-22 00:51:17 +0530

    main runner file added

--- java_dsa/Arrays/LongestSubarray.java
+++ java_dsa/Arrays/LongestSubarray.java
@@ -1,27 +1,29 @@
 package Arrays;
 
 
+import java.util.HashMap;
+
 public class LongestSubarray {
     public static void main(String[] args){
         System.out.println("Longest SubArray Program Start");
 
-        int[] arr = {2,-1,1,-1,1,2,1};
+        int[] arr = {1,2,3,1,1,1,1,4,2,3};
         int k=1;
-        int tempSum =  0;
+
+        HashMap<Integer,Integer> myMap = new HashMap<>();
+
+        int prefixSum =  0;
         int tempLength = 0;
         int maxLength = 0;
 
-        for (int element : arr) {
-            tempSum += element;
-            tempLength += 1;
-            if(tempSum == k){
-                if(tempLength > maxLength){
-                    maxLength = tempLength;
+       for(int i = 0;i<arr.length;i++){
+           prefixSum += arr[i];
+           if(prefixSum == k){
+               tempLength = i+1; //jitna index of array hoga usme +1 kr denge to size of subarray mil jayega
+           }
+           System.out.println(arr[i]);
 
-                }
-                tempLength = 0;
-            }
-        }
+       }
 
         System.out.println("Longest Subarray is: "+maxLength);
 
